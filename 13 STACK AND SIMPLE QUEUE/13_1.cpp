0316089b91f05508c97f089a04489ea37d4803ad
Pritesh Suthar <124127811+priteshsuthar247@users.noreply.github.com>
2023-04-26 13:34:37 +0530

    Add files via upload

---
+++ 13 STACK AND SIMPLE QUEUE/13_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() 
+{
+    stack<string> lang;
+    lang.push("Java");
+    lang.push("Python");
+    lang.push("C++");
+    cout<<lang.top();
+    return 0;
+}
