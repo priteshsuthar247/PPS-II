8ab6cce6c510864e229f37a766ba245683cfe7ec
Pritesh Suthar <124127811+priteshsuthar247@users.noreply.github.com>
2023-04-26 13:33:51 +0530

    Add files via upload

---
+++ 11 FILE HANDELING/11_1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main() 
+{
+  char ch[10];
+  ofstream f1;
+  f1.open("country.txt");
+  f1<<"India"<<endl;
+  f1<<"Russia"<<endl;
+  f1.open("capital.txt");
+  f1<<"Delhi"<<endl;
+  f1<<"Moscow"<<endl;
+  ifstream f2("country.txt");
+  cout<<"Country: "<<endl;
+  cout<<f2.rdbuf();
+  f2.close();
+  ifstream f3("capital.txt");
+  cout<<"Capital:"<<endl;
+  cout<<f3.rdbuf();
+  f3.close();
+  return 0;
+}
